e7e7efa02177552b584099d6e9eb644216759fed
TheoDUT <theo.dutilleul@edu.devinci.fr>
2024-01-29 15:37:47 +0100

    I added two questions

--- questions.py
+++ questions.py
@@ -2,4 +2,7 @@ sample_questions = [
     {"text": "What is the capital of France?", "answer": "Paris"},
     {"text": "Which planet is known as the Red Planet?", "answer": "Mars"},
     {"text": "What is 2 + 2?", "answer": "4"},
+    {"text": "Who wrote Romeo and Juliet ? (full name) ", "asnwer": "William Shakespeare" },
+    {"text": "Who was the first president of the United States ? (full name)", "answer":"George Washington"},
+
 ]
