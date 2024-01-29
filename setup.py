1945830bf6c5a3a877188129062732a53865ccef
doriandulor <151473334+doriandulor@users.noreply.github.com>
2024-01-29 14:04:41 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{Quiz_game-Dulor_Dorian-CDOF4}',
+ version='1.0',
+ author='{doriandulor}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
